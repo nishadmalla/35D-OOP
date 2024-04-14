980b4b00f7ddfc911e8f38dd62470d3f7f1a93c9
nishad <me0again0@gmail.com>
2024-04-14 12:17:25 +0545

    Week one  comment

---
+++ Week1/hello.java
@@ -0,0 +1,8 @@
+package task.Week1;
+
+public class hello {
+    public static void main(String[] args) {
+        // System.out.println("Helloworld");
+        System.out.println( "this is my first program");
+    }
+}
