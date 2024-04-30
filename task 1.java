83977b508aa7691bd42f9ec843be49f7845cbd53
nishadmalla <149762693+nishadmalla@users.noreply.github.com>
2024-04-30 22:22:18 +0545

    Add files via upload

---
+++ task 1.java
@@ -0,0 +1,29 @@
+package task.Week1;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+        System.out.print( "this is my first program\ni am on module st4003cem ");
+        System.out.println("A \"quoted\" String is\n'much'better isf you learn\nthe rules of\"escape siquence\"\nAlso \"\" representation an empty string\nString. Don't forget: use \\\"instead of \" \n \"is not the same as\"");
+        System.out.print("*\n**\n***\n****\n");
+        System.out.print("*********\n*********\n*********\n*********");
+        System.out.println("well-formed Java program has a main method with { and } braces.\n\nSystem.out.println statementA  has ( and ) and usually a String that starts and ends \nwith a \" character. (But we type \\\" instead!)\nWhat is the difference between\na ' and a \"?  Or between a \" and a \\\"?\n\nOne is what we see when we're typing our program. The other is what appears on\nthe \"console.\"");
+    }
+}
+// A well-formed Java program has a main method with { and } braces.
+
+
+
+// System.out.println statementA  has ( and ) and usually a String that starts and ends with a " character.
+
+// (But we type \" instead!)
+
+// What is the difference between
+
+// a ' and a "?  Or between a " and a \"?
+
+
+
+// One is what we see when we're typing our program. The other is what appears on the "console."
+
+
